99c7530c7d85aaa292f5a53945af21007b488162
Adesewa Aishat <adesewaaishat@gmail.com>
2023-06-19 16:48:26 +0100

    fixed the code to fit instructions

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-"use strict"
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+'use strict';
+const hours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+];
 
 const seattle = {
   storeName: 'seattle',
@@ -9,25 +24,30 @@ const seattle = {
   customersEachHour: [23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23],
   cookiesEachHour: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57],
   totalDailyCookies: 875,
-  getCustomers: function (){
-    const customerperhour = randomCustomers(this.minCustPerHour, this.maxCustPerHour) + ' people'
+  getCustomers: function () {
+    const customerperhour =
+      randomCustomers(this.minCustPerHour, this.maxCustPerHour) + ' people';
     console.log(customerperhour);
   },
-  render : function (){
-    const article = document.createElement('article')
+  render: function () {
+    const article = document.createElement('article');
     const parentElement = document.getElementById('kittenProfiles');
     parentElement.appendChild(article);
     const h3 = document.createElement('h3');
     article.appendChild(h3);
-    const ul = document.createElement('ul')
+    const ul = document.createElement('ul');
     article.appendChild(ul);
     for (let i = 0; i < hours.length; i++) {
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       li.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
       ul.appendChild(li);
-  }
-}
-}
+    }
+    const li = document.createElement('li')
+    ul.appendChild(li);
+    // <p>Total: 875 cookies</p>
+    li.textContent = 'Total: ' + this.totalDailyCookies + ' cookies'
+  },
+};
 
 function randomCustomers(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
