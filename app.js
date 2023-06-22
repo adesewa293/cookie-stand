93c509fbcdd4f1c43dcb1723bcffaff508e2339c
Adesewa Aishat <adesewaaishat@gmail.com>
2023-06-22 12:44:33 +0100

    adjusted html

--- app.js
+++ app.js
@@ -1,61 +1,4 @@
-// 'use strict';
-// const hours = [
-//   '6am',
-//   '7am',
-//   '8am',
-//   '9am',
-//   '10am',
-//   '11am',
-//   '12pm',
-//   '1pm',
-//   '2pm',
-//   '3pm',
-//   '4pm',
-//   '5pm',
-//   '6pm',
-//   '7pm',
-// ];
-
-// const seattle = {
-//   storeName: 'seattle',
-//   minCustPerHour: 23,
-//   maxCustPerHour: 65,
-//   avgCookiesPerHour: 6.3,
-//   customersEachHour: [23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23],
-//   cookiesEachHour: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57],
-//   totalDailyCookies: 875,
-//   generateCustomersPerHour: function () {
-//     const customerperhour =
-//       getRandomNumber(this.minCustPerHour, this.maxCustPerHour) + ' people';
-//     console.log(customerperhour);
-//   },
-//   render: function () {
-//     const article = document.createElement('article');
-//     const parentElement = document.getElementById('cookieData');
-//     parentElement.appendChild(article);
-//     const h3 = document.createElement('h3');
-//     article.appendChild(h3);
-//     const ul = document.createElement('ul');
-//     article.appendChild(ul);
-//     for (let i = 0; i < hours.length; i++) {
-//       const li = document.createElement('li');
-//       li.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
-//       ul.appendChild(li);
-//     }
-//     const li = document.createElement('li');
-//     ul.appendChild(li);
-//     li.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-//   },
-// };
-
-// function randomCustomers(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// seattle.generateCustomersPerHour();
-// seattle.render();
-
-function storeLocations(storename, openHours, ContactInfo, AdditionalInfo) {
+function StoreLocations(storename, openHours, ContactInfo, AdditionalInfo) {
   this.storename = storename;
   this.openHours = openHours;
   this.ContactInfo = ContactInfo;
@@ -63,7 +6,7 @@ function storeLocations(storename, openHours, ContactInfo, AdditionalInfo) {
   this.render();
 }
 
-storeLocations.prototype.render = function () {
+StoreLocations.prototype.render = function () {
   const containerElement = document.getElementById("storeLocationContainer");
   const article = document.createElement("article");
   containerElement.appendChild(article);
@@ -81,33 +24,33 @@ storeLocations.prototype.render = function () {
   article.appendChild(p3);
 };
 
-const seattle = new storeLocations(
+const seattle = new StoreLocations(
   "Seattle",
   "Monday - Friday: 9:00 AM - 6:00 PM Saturday: 10:00 AM - 4:00 PM Sunday: Closed",
   "123-456-7890 Email: store1@example.com",
   "Main Street City, State ZIP"
 );
-const tokyo = new storeLocations(
+const tokyo = new StoreLocations(
   "Tokyo",
   "Monday - Friday: 9:00 AM - 6:00 PM Saturday: 10:00 AM - 4:00 PM Sunday: Closed",
   "123-456-7890 Email: store1@example.com",
   "456 Oak AvenueCity, State ZIP"
 );
-const dubai = new storeLocations(
+const dubai = new StoreLocations(
   "Dubai",
   "Monday - Friday: 9:00 AM - 6:00 PM Saturday: 10:00 AM - 4:00 PM Sunday: Closed",
   "123-456-7890 Email: store1@example.com",
   "456 Oak AvenueCity, State ZIP"
 );
 
-const paris = new storeLocations(
+const paris = new StoreLocations(
   "Paris",
   "Monday - Friday: 9:00 AM - 6:00 PM Saturday: 10:00 AM - 4:00 PM Sunday: Closed",
   "123-456-7890 Email: store1@example.com",
   "456 Oak AvenueCity, State ZIP"
 );
 
-const lima = new storeLocations(
+const lima = new StoreLocations(
   "Lima",
   "Monday - Friday: 9:00 AM - 6:00 PM Saturday: 10:00 AM - 4:00 PM Sunday: Closed",
   "123-456-7890 Email: store1@example.com",
